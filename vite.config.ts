0007a8b6e19cb8ed3b7d6c6148f495f1d15e7cb0
jyonny32 <jonesjonathan206@gmail.com>
2025-08-12 15:39:54 -0500

    Fix Vite alias for @ path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
 export default defineConfig({
   base: '/soku/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: { outDir: 'dist' }
 })
